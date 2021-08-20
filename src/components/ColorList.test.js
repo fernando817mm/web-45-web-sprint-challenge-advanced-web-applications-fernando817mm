c5fad6c847bf0fdc4b0fc94905688f59dc5ef428
Fernando Martinez <fernando817mm@gmail.com>
2021-08-20 16:45:53 -0500

    colorlist tests done

--- src/components/ColorList.test.js
+++ src/components/ColorList.test.js
@@ -4,11 +4,24 @@ import MutationObserver from 'mutationobserver-shim';
 import { render, screen} from "@testing-library/react";
 import ColorList from './ColorList';
 
+//<ColorList colors={colors} editing={editing} toggleEdit={toggleEdit} saveEdit={saveEdit} deleteColor={deleteColor}/>
+const colors = [ { color: "aliceblue", code: { hex: "#f0f8ff" }, id: 1}, { color: "limegreen", code: { hex: "#99ddbc"}, id: 2} ];
+
 test("Renders an empty list of colors without errors", () => {
+    render(<ColorList colors={[]}/>);
 });
 
+
 test("Renders a list of colors without errors", () => {
+    render(<ColorList colors={colors}/>)
 });
 
 test("Renders the EditForm when editing = true and does not render EditForm when editing = false", () => {
+    const { rerender } = render(<ColorList colors={colors} editing={true}/>);
+
+    const editForm = screen.queryByText(/edit color/i);
+    expect(editForm).toBeInTheDocument();
+
+    rerender(<ColorList colors={colors} editing={false}/>);
+    expect(editForm).not.toBeInTheDocument();
 });
