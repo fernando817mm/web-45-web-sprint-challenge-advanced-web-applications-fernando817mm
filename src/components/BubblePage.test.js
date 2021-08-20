b77efd5a637cf2684a02da32bf7d73fbad64387b
Fernando Martinez <fernando817mm@gmail.com>
2021-08-20 17:11:51 -0500

    all but last test done

--- src/components/BubblePage.test.js
+++ src/components/BubblePage.test.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import MutationObserver from 'mutationobserver-shim';
+// import * as axios from 'axios';
 
 import { render, screen} from "@testing-library/react";
 import BubblePage from './BubblePage';
 
+// jest.mock('axios');
+
+// const colors = [ { color: "aliceblue", code: { hex: "#f0f8ff" }, id: 1}, { color: "limegreen", code: { hex: "#99ddbc"}, id: 2} ];
+
 test("Renders without errors", ()=> {
-    
+    render(<BubblePage/>);
 });
 
 test("Renders appropriate number of colors passed in through mock", async ()=> {
     //Keep in mind that our service is called on mount for this component.
+    render(<BubblePage/>);
+
 });
